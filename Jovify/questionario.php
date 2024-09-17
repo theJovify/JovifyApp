1577fa11dbc26a13b030c56215a7f7d26e569e9d
JGabrielFDC <jgabrielfdc08@gmail.com>
2024-09-16 21:52:24 -0300

    -Reformulação Layouts e Perguntas

---
+++ Jovify/questionario.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Questionário de Características</title>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" />
+  <link rel="stylesheet" href="public/style.css" />
+  <style></style>
+</head>
+
+<body class="questionario">
+  <center>
+    <div class="container w-100 h-100 my-auto">
+      <form action='index.html' method="POST">
+
+        <!-- * Questão 1 * -->
+        <div class="question">
+          <h3>
+            Você gosta de planejar as coisas ?
+          </h3>
+          <div class="options">
+            <span class="direction-left">Discordo</span>
+            <label>
+              <input type="radio" name="q1" value="1" />
+              <span>1</span>
+            </label>
+            <label>
+              <input type="radio" name="q1" value="2" />
+              <span>2</span>
+            </label>
+            <label>
+              <input type="radio" name="q1" value="3" />
+              <span>3</span>
+            </label>
+            <label>
+              <input type="radio" name="q1" value="4" />
+              <span>4</span>
+            </label>
+            <label>
+              <input type="radio" name="q1" value="5" />
+              <span>5</span>
+            </label>
+            <span class="direction-right">Concordo</span>
+          </div>
+        </div>
+
+        <!-- * Questão 2 * -->
+        <div class="question">
+          <h3>
+            Você se sente bem ao resolver problemas complexos ?
+          </h3>
+          <div class="options">
+            <span class="direction-left">Discordo</span>
+            <label>
+              <input type="radio" name="q2" value="1" />
+              <span>1</span>
+            </label>
+            <label>
+              <input type="radio" name="q2" value="2" />
+              <span>2</span>
+            </label>
+            <label>
+              <input type="radio" name="q2" value="3" />
+              <span>3</span>
+            </label>
+            <label>
+              <input type="radio" name="q2" value="4" />
+              <span>4</span>
+            </label>
+            <label>
+              <input type="radio" name="q2" value="5" />
+              <span>5</span>
+            </label>
+            <span class="direction-right">Concordo</span>
+          </div>
+        </div>
+
+        <!-- * Questão 3 * -->
+        <div class="question">
+          <h3>
+            Você se importa em entender os sentimentos dos outros ?
+          </h3>
+          <div class="options">
+            <span class="direction-left">Discordo</span>
+            <label>
+              <input type="radio" name="q3" value="1" />
+              <span>1</span>
+            </label>
+            <label>
+              <input type="radio" name="q3" value="2" />
+              <span>2</span>
+            </label>
+            <label>
+              <input type="radio" name="q3" value="3" />
+              <span>3</span>
+            </label>
+            <label>
+              <input type="radio" name="q3" value="4" />
+              <span>4</span>
+            </label>
+            <label>
+              <input type="radio" name="q3" value="5" />
+              <span>5</span>
+            </label>
+            <span class="direction-right">Concordo</span>
+          </div>
+        </div>
+
+        <!-- * Questão 4 * -->
+        <div class="question">
+          <h3>
+            Você considera importante construir relacionamentos profundos ?
+          </h3>
+          <div class="options">
+            <span class="direction-left">Discordo</span>
+            <label>
+              <input type="radio" name="q4" value="1" />
+              <span>1</span>
+            </label>
+            <label>
+              <input type="radio" name="q4" value="2" />
+              <span>2</span>
+            </label>
+            <label>
+              <input type="radio" name="q4" value="3" />
+              <span>3</span>
+            </label>
+            <label>
+              <input type="radio" name="q4" value="4" />
+              <span>4</span>
+            </label>
+            <label>
+              <input type="radio" name="q4" value="5" />
+              <span>5</span>
+            </label>
+            <span class="direction-right">Concordo</span>
+          </div>
+        </div>
+
+        <!-- * Questão 5 * -->
+        <div class="question">
+          <h3>
+            É importante para você seguir regras e normas ?
+          </h3>
+          <div class="options">
+            <span class="direction-left">Discordo</span>
+            <label>
+              <input type="radio" name="q5" value="1" />
+              <span>1</span>
+            </label>
+            <label>
+              <input type="radio" name="q5" value="2" />
+              <span>2</span>
+            </label>
+            <label>
+              <input type="radio" name="q5" value="3" />
+              <span>3</span>
+            </label>
+            <label>
+              <input type="radio" name="q5" value="4" />
+              <span>4</span>
+            </label>
+            <label>
+              <input type="radio" name="q5" value="5" />
+              <span>5</span>
+            </label>
+            <span class="direction-right">Concordo</span>
+          </div>
+        </div>
+
+        <!-- * Questão 6 * -->
+        <div class="question">
+          <h3>
+            Você se sente bem ao proteger o que é importante para você ?
+          </h3>
+          <div class="options">
+            <span class="direction-left">Discordo</span>
+            <label>
+              <input type="radio" name="q6" value="1" />
+              <span>1</span>
+            </label>
+            <label>
+              <input type="radio" name="q6" value="2" />
+              <span>2</span>
+            </label>
+            <label>
+              <input type="radio" name="q6" value="3" />
+              <span>3</span>
+            </label>
+            <label>
+              <input type="radio" name="q6" value="4" />
+              <span>4</span>
+            </label>
+            <label>
+              <input type="radio" name="q6" value="5" />
+              <span>5</span>
+            </label>
+            <span class="direction-right">Concordo</span>
+          </div>
+        </div>
+
+        <!-- * Questão 7 * -->
+        <div class="question">
+          <h3>
+            Você se sente bem ao ter novas experiências ?
+          </h3>
+          <div class="options">
+            <span class="direction-left">Discordo</span>
+            <label>
+              <input type="radio" name="q7" value="1" />
+              <span>1</span>
+            </label>
+            <label>
+              <input type="radio" name="q7" value="2" />
+              <span>2</span>
+            </label>
+            <label>
+              <input type="radio" name="q7" value="3" />
+              <span>3</span>
+            </label>
+            <label>
+              <input type="radio" name="q7" value="4" />
+              <span>4</span>
+            </label>
+            <label>
+              <input type="radio" name="q7" value="5" />
+              <span>5</span>
+            </label>
+            <span class="direction-right">Concordo</span>
+          </div>
+        </div>
+
+        <!-- * Questão 8 * -->
+        <div class="question">
+          <h3>
+            Você se sente bem explorando ideias novas e diferentes ?
+          </h3>
+          <div class="options">
+            <span class="direction-left">Discordo</span>
+            <label>
+              <input type="radio" name="q8" value="1" />
+              <span>1</span>
+            </label>
+            <label>
+              <input type="radio" name="q8" value="2" />
+              <span>2</span>
+            </label>
+            <label>
+              <input type="radio" name="q8" value="3" />
+              <span>3</span>
+            </label>
+            <label>
+              <input type="radio" name="q8" value="4" />
+              <span>4</span>
+            </label>
+            <label>
+              <input type="radio" name="q8" value="5" />
+              <span>5</span>
+            </label>
+            <span class="direction-right">Concordo</span>
+          </div>
+        </div>
+
+        <button class="submit-button" type="submit">Enviar</button>
+      </form>
+    </div>
+  </center>
+</body>
+
+</html>
